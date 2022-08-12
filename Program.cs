6503e4422ef50a52f2e0d0a8667eb55c0be19f3a
Roman <yuuziklol@gmail.com>
2022-08-12 14:46:44 +0700

    add CountElements

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿string [] InputArray = {"Moscow2", "ks3", "cow", "k:52", "Silense", "car", "home", "cs", "a", "425light"};
+
+int CountElements(string [] array)
+{
+    int count = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i = i+1)
+    {
+        if (array[i].Length <= 3)
+        {
+            count = count + 1;
+        }
+    }
+    return(count);
+}
+
+
+
